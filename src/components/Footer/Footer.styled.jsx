ec02b99d4d25c0bca550529b3048944ff2ec9f42
barkode <konstantinks@gmail.com>
2023-12-11 10:07:56 +0000

    Initialize styles

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  width: 100%;
   height: 276px;
+  padding: 50px 120px 50px 120px;
   background-color: rgba(27, 82, 100, 0.8);
 `;
