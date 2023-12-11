67017a479905bda3596a57acddd4a700836d8d8f
barkode <konstantinks@gmail.com>
2023-12-11 10:07:42 +0000

    Add categories

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,33 +4,35 @@ export const Footer = () => {
   return (
     <footer>
       <Container>
-        <p>SPORTSVIT</p>
+        <div>SPORTSVIT</div>
         <div>
-          <ul>
-            <li>інформація</li>
-            <li>Доставка та оплата</li>
-            <li>Повернення та обмін</li>
-            <li>Угода користувача</li>
-            <li>Питання та відповіді</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>магазин</li>
-            <li>Новинки</li>
-            <li>Жінки</li>
-            <li>Чоловіки</li>
-            <li>Знижки</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>контакти</li>
-            <li>+38(099)4567895</li>
-            <li>sportsvit@gmail.com</li>
-            <li>Facebook</li>
-            <li>Instagram</li>
-          </ul>
+          <div>
+            <ul>
+              <li>інформація</li>
+              <li>Доставка та оплата</li>
+              <li>Повернення та обмін</li>
+              <li>Угода користувача</li>
+              <li>Питання та відповіді</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>магазин</li>
+              <li>Новинки</li>
+              <li>Жінки</li>
+              <li>Чоловіки</li>
+              <li>Знижки</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>контакти</li>
+              <li>+38(099)4567895</li>
+              <li>sportsvit@gmail.com</li>
+              <li>Facebook</li>
+              <li>Instagram</li>
+            </ul>
+          </div>
         </div>
       </Container>
     </footer>
