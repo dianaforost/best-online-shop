9d73a4dc0b2367cc9e02ea054431220aa946b0ef
barkode <konstantinks@gmail.com>
2023-12-11 00:44:38 +0000

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+export const Header = () => {
+  return (
+    <>
+      <div>SportSvit</div>
+      <div>
+        <ul>
+          <li>НОВИНКИ</li>
+          <li>КАТАЛОГ</li>
+          <li>ЖІНКИ</li>
+          <li>ЧОЛОВІКИ</li>
+          <li>ЗНИЖКИ</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>search</li>
+          <li>user</li>
+          <li>favorite</li>
+          <li>cart</li>
+          <li>language</li>
+        </ul>
+      </div>
+    </>
+  );
+};
