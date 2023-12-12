0bffdacbface503240cbfb41996f31cba1e62ac1
barkode <konstantinks@gmail.com>
2023-12-12 00:54:09 +0000

    Add reexport components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+// Icons
+export { IconEnvelope } from './IconEnvelope/IconEnvelope';
+export { IconFacebook } from './IconFacebook/IconFacebook';
+export { IconHeart } from './IconHeart/IconHeart';
+export { IconInstagram } from './IconInstagram/IconInstagram';
+export { IconLanguage } from './IconLanguage/IconLanguage';
+export { IconPhone } from './IconPhone/IconPhone';
+export { IconSearch } from './IconSearch/IconSearch';
+export { IconShoppingCart } from './IconShoppingCart/IconShoppingCart';
+export { IconUser } from './IconUser/IconUser';
+
+// Components
+
+export { SharedLayout } from './SharedLayout/SharedLayout';
+export { Header } from './Header/Header';
+export { GenderSection } from './GenderSection/GenderSection';
+export { Footer } from './Footer/Footer';
