6ab3665bf091fb9add0aa2dad2338d07415dd175
barkode <konstantinks@gmail.com>
2023-12-12 01:11:03 +0000

    add icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,40 +1,60 @@
+import {
+  IconEnvelope,
+  IconFacebook,
+  IconInstagram,
+  IconPhone,
+} from 'components';
 import { Container } from './Footer.styled';
 
 export const Footer = () => {
   return (
-    <footer>
-      <Container>
-        <div>SPORTSVIT</div>
-        <div>
+    <>
+      <footer>
+        <Container>
+          <div>SPORTSVIT</div>
           <div>
-            <ul>
-              <li>інформація</li>
-              <li>Доставка та оплата</li>
-              <li>Повернення та обмін</li>
-              <li>Угода користувача</li>
-              <li>Питання та відповіді</li>
-            </ul>
+            <div>
+              <ul>
+                <li>інформація</li>
+                <li>Доставка та оплата</li>
+                <li>Повернення та обмін</li>
+                <li>Угода користувача</li>
+                <li>Питання та відповіді</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>магазин</li>
+                <li>Новинки</li>
+                <li>Жінки</li>
+                <li>Чоловіки</li>
+                <li>Знижки</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>контакти</li>
+                <li>
+                  <IconPhone />
+                  <a href="tel:+380994567895">+38(099)4567895</a>
+                </li>
+                <li>
+                  <IconEnvelope />
+                  <a href="email:sportsvit@gmail.com">sportsvit@gmail.com</a>
+                </li>
+                <li>
+                  <IconFacebook />
+                  <a href="/">facebook</a>
+                </li>
+                <li>
+                  <IconInstagram />
+                  <a href="/">instagram</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <ul>
-              <li>магазин</li>
-              <li>Новинки</li>
-              <li>Жінки</li>
-              <li>Чоловіки</li>
-              <li>Знижки</li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li>контакти</li>
-              <li>+38(099)4567895</li>
-              <li>sportsvit@gmail.com</li>
-              <li>Facebook</li>
-              <li>Instagram</li>
-            </ul>
-          </div>
-        </div>
-      </Container>
-    </footer>
+        </Container>
+      </footer>
+    </>
   );
 };
