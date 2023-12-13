ceabe2ce227df0eb6978af4ed03878e01f105688
barkode <konstantinks@gmail.com>
2023-12-13 23:56:34 +0000

    Delete container

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from 'components/Footer/Footer';
 import { Header } from 'components/Header/Header';
-import { Container } from './SharedLayout.styled';
+// import { Container } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
   return (
-    <Container>
+    <>
       <Header />
       <Outlet />
       <Footer />
-    </Container>
+    </>
   );
 };
