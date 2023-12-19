a67081f09a4c062ca989397f824678b401d71466
barkode <konstantinks@gmail.com>
2023-12-19 00:34:20 +0000

    Create Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,3 @@
+export const Hero = () => {
+  return <h2>IT IS HERO SECTION</h2>;
+};
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import { GenderSection } from 'components/GenderSection/GenderSection';
+import { Hero } from 'components/Hero/Hero';
 import { ProductsSlider } from 'components/ProductsSlider/ProductsSlider';
 
 export const MainPage = () => {
   return (
     <>
       <main>
+        <Hero />
         <GenderSection />
         <ProductsSlider type={'new'} />
         <GenderSection type={'winter'} />
