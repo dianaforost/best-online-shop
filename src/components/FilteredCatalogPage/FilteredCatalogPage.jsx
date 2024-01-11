74b560a45751167dc13ed2ec26d673003acd9552
Diana Forostiana <dianaforost@gmail.com>
2024-01-11 11:07:16 +0100

    feat: catalogPage

--- src/components/FilteredCatalogPage/FilteredCatalogPage.jsx
+++ src/components/FilteredCatalogPage/FilteredCatalogPage.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import {
   ButtonList,
+  CategoriesList,
   Container,
+  ProductsItem,
+  ProductsList,
+  ProductsTitle,
   Section,
   Title,
 } from './FilteredCatalogPage.styled';
 
 export const FilteredCatalogPage = ({ category }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  const toggleShowSearch = () => {
+    setIsShown(!isShown);
+  };
+
   const categoryTitles = {
     women: 'Одяг для жінки',
     men: 'Одяг для чоловіка',
@@ -14,12 +25,58 @@ export const FilteredCatalogPage = ({ category }) => {
   };
   const categories = ['Футболки', 'Топи', 'Шорти', 'Штани', 'Костюми'];
   const title = categoryTitles[category];
+  const products = [
+    {
+      title: 'Жіночі спортивні штани джогери-фіолетовий',
+      price: '940 ₴',
+    },
+    {
+      title: 'Жіночі спортивні штани джогери-фіолетовий',
+      price: '940 ₴',
+    },
+    {
+      title: 'Жіночі спортивні штани джогери-фіолетовий',
+      price: '940 ₴',
+    },
+  ];
 
   return (
     <Section>
       <Container>
         <Title>{title}</Title>
-        <ButtonList>Категорії</ButtonList>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <ButtonList onClick={toggleShowSearch}>Категорії</ButtonList>
+            <CategoriesList $isVisible={isShown}>
+              {categories.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </CategoriesList>
+          </div>
+          <ProductsList>
+            {products.map(item => (
+              <ProductsItem>
+                <div
+                  style={{
+                    width: '282px',
+                    height: '300px',
+                    backgroundColor: '#D9D9D9',
+                  }}
+                ></div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '8px',
+                  }}
+                >
+                  <ProductsTitle>{item.title}</ProductsTitle>
+                  <p>{item.price}</p>
+                </div>
+              </ProductsItem>
+            ))}
+          </ProductsList>
+        </div>
       </Container>
     </Section>
   );
--- src/components/FilteredCatalogPage/FilteredCatalogPage.styled.jsx
+++ src/components/FilteredCatalogPage/FilteredCatalogPage.styled.jsx
@@ -39,3 +39,35 @@ export const ButtonList = styled.button`
   @media (min-width: 768px) {
   }
 `;
+export const CategoriesList = styled.div`
+  display: ${({ $isVisible }) => ($isVisible ? 'flex' : 'none')};
+  margin-top: ${({ $isVisible }) => ($isVisible ? '20px' : '0')};
+  flex-direction: column;
+  gap: 20px;
+  font-size: 26px;
+  line-height: 33px;
+`;
+export const ProductsList = styled.ul`
+  gap: 24px;
+  display: flex;
+  @media (min-width: 768px) {
+  }
+`;
+export const ProductsItem = styled.li`
+  max-width: 282px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  @media (min-width: 768px) {
+  }
+`;
+export const ProductsTitle = styled.h3`
+  font-family: Mulish;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  @media (min-width: 768px) {
+  }
+`;
