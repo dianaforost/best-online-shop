e83429f977495f3ce2a1c49bb88b120915a06f26
barkode <konstantinks@gmail.com>
2024-01-12 23:04:31 +0000

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 // Icons
-export * from './IconEnvelope/IconEnvelope';
-export * from './IconFacebook/IconFacebook';
+// export * from './IconEnvelope/IconEnvelope';
+// export * from './IconFacebook/IconFacebook';
 export * from './IconFavorite/IconFavorite';
-export * from './IconInstagram/IconInstagram';
+// export * from './IconInstagram/IconInstagram';
 export * from './IconLanguage/IconLanguage';
-export * from './IconPhone/IconPhone';
+// export * from './IconPhone/IconPhone';
 export * from './IconSearch/IconSearch';
 export * from './IconShoppingCart/IconShoppingCart';
 export * from './IconUser/IconUser';
