65d953b018fe2663606be8f69b83e188fc40b17b
barkode <konstantinks@gmail.com>
2024-02-02 22:41:37 +0000

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,58 +25,60 @@ export const Header = () => {
   };
 
   return (
-    <Container>
-      <LogoText>
-        <NavLink to={'/'}>SportSvit</NavLink>
-      </LogoText>
-      <div>
-        <nav>
-          <HeaderStoreNavMenu>
-            <HeaderStoreNavItem>
-              <NavLink to={'/catalog/novelty'}>Новинки</NavLink>
-            </HeaderStoreNavItem>
-            <HeaderStoreNavItem>
-              <NavLink to={'/catalog'}>Каталог</NavLink>
-            </HeaderStoreNavItem>
-            <HeaderStoreNavItem>
-              <NavLink to={'/catalog/women'}>Жінки</NavLink>
-            </HeaderStoreNavItem>
-            <HeaderStoreNavItem>
-              <NavLink to={'/catalog/man'}>Чоловіки</NavLink>
-            </HeaderStoreNavItem>
-            <HeaderStoreNavItem>
-              <NavLink to={'/deals'}>Знижки</NavLink>
-            </HeaderStoreNavItem>
-          </HeaderStoreNavMenu>
-        </nav>
-      </div>
-      <div>
-        <HeaderIconMenuNav>
-          {isToggled && (
-            <label>
-              <input type="text" value={inputField} onChange={handleInput} />
-            </label>
-          )}
-          <HeaderIconMenuItem onClick={handleToggle} type="button">
-            <Icon id={'search'} width={'24px'} height={'24px'} />
-          </HeaderIconMenuItem>
-          <HeaderIconMenuItem>
-            <a href="/">
-              <Icon id={'user'} width={'24px'} height={'24px'} />
-            </a>
-          </HeaderIconMenuItem>
-          <HeaderIconMenuItem>
-            <a href="/">
-              <Icon id={'heart'} width={'24px'} height={'24px'} />
-            </a>
-          </HeaderIconMenuItem>
-          <HeaderIconMenuItem>
-            <a href="/">
-              <Icon id={'shopping-cart'} width={'24px'} height={'24px'} />
-            </a>
-          </HeaderIconMenuItem>
-        </HeaderIconMenuNav>
-      </div>
-    </Container>
+    <header>
+      <Container>
+        <LogoText>
+          <NavLink to={'/'}>SportSvit</NavLink>
+        </LogoText>
+        <div>
+          <nav>
+            <HeaderStoreNavMenu>
+              <HeaderStoreNavItem>
+                <NavLink to={'/catalog/novelty'}>Новинки</NavLink>
+              </HeaderStoreNavItem>
+              <HeaderStoreNavItem>
+                <NavLink to={'/catalog'}>Каталог</NavLink>
+              </HeaderStoreNavItem>
+              <HeaderStoreNavItem>
+                <NavLink to={'/catalog/women'}>Жінки</NavLink>
+              </HeaderStoreNavItem>
+              <HeaderStoreNavItem>
+                <NavLink to={'/catalog/man'}>Чоловіки</NavLink>
+              </HeaderStoreNavItem>
+              <HeaderStoreNavItem>
+                <NavLink to={'/deals'}>Знижки</NavLink>
+              </HeaderStoreNavItem>
+            </HeaderStoreNavMenu>
+          </nav>
+        </div>
+        <div>
+          <HeaderIconMenuNav>
+            {isToggled && (
+              <label>
+                <input type="text" value={inputField} onChange={handleInput} />
+              </label>
+            )}
+            <HeaderIconMenuItem onClick={handleToggle} type="button">
+              <Icon id={'search'} width={'24px'} height={'24px'} />
+            </HeaderIconMenuItem>
+            <HeaderIconMenuItem>
+              <a href="/">
+                <Icon id={'user'} width={'24px'} height={'24px'} />
+              </a>
+            </HeaderIconMenuItem>
+            <HeaderIconMenuItem>
+              <a href="/">
+                <Icon id={'heart'} width={'24px'} height={'24px'} />
+              </a>
+            </HeaderIconMenuItem>
+            <HeaderIconMenuItem>
+              <a href="/">
+                <Icon id={'shopping-cart'} width={'24px'} height={'24px'} />
+              </a>
+            </HeaderIconMenuItem>
+          </HeaderIconMenuNav>
+        </div>
+      </Container>
+    </header>
   );
 };
