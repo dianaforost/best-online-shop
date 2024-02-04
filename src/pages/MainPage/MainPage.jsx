bc9aae9d9ec458038e3b10196169931949c7b9bf
barkode <konstantinks@gmail.com>
2024-02-04 16:30:05 +0000

    add banner sliders

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,8 @@
-import { BannerSlider } from 'components/BannerSlider/BannerSlider';
+import { BannerBottomComponent, BannerTopComponent } from 'components';
 import { GenderSection } from 'components/GenderSection/GenderSection';
 import { MainSection } from 'components/MainSection/MainSection';
 import { ProductsSlider } from 'components/ProductsSlider/ProductsSlider';
+import { banner1, banner1x2, banner2, banner2x2 } from 'images';
 
 export const MainPage = () => {
   return (
@@ -9,9 +10,10 @@ export const MainPage = () => {
       <main>
         <MainSection />
         <GenderSection />
+        <BannerTopComponent bannerimg={banner1} bannerimg2x={banner1x2} />
         <ProductsSlider type={'new'} />
         <GenderSection type={'winter'} />
-        <BannerSlider />
+        <BannerBottomComponent bannerimg={banner2} bannerimg2x={banner2x2} />
         <ProductsSlider type={'discount'} />
       </main>
     </>
