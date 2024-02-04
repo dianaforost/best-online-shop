39712366be17f12ae3b161f73338218824030360
barkode <konstantinks@gmail.com>
2024-02-04 17:53:44 +0000

    Update MainPage.jsx

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import { BannerBottomComponent, BannerTopComponent } from 'components';
 import { GenderSection } from 'components/GenderSection/GenderSection';
 import { MainSection } from 'components/MainSection/MainSection';
 import { ProductsSlider } from 'components/ProductsSlider/ProductsSlider';
-import { banner1, banner1x2, banner2, banner2x2 } from 'images';
+import { bannerTop, bannerTop2x, bannerBottom, bannerBottom2x } from 'images';
 
 export const MainPage = () => {
   return (
@@ -10,10 +10,13 @@ export const MainPage = () => {
       <main>
         <MainSection />
         <GenderSection />
-        <BannerTopComponent bannerimg={banner1} bannerimg2x={banner1x2} />
+        <BannerTopComponent $bannerimg={bannerTop} $bannerimg2x={bannerTop2x} />
         <ProductsSlider type={'new'} />
         <GenderSection type={'winter'} />
-        <BannerBottomComponent bannerimg={banner2} bannerimg2x={banner2x2} />
+        <BannerBottomComponent
+          $bannerimg={bannerBottom}
+          $bannerimg2x={bannerBottom2x}
+        />
         <ProductsSlider type={'discount'} />
       </main>
     </>
