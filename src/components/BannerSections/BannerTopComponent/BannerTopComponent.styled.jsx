0379da8f9de33976661db4febbb7671edb63ca24
barkode <konstantinks@gmail.com>
2024-02-04 20:31:54 +0000

    Add text

--- src/components/BannerSections/BannerTopComponent/BannerTopComponent.styled.jsx
+++ src/components/BannerSections/BannerTopComponent/BannerTopComponent.styled.jsx
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 export const BannerHeader = styled.h2`
   max-width: 620px;
+  height: 160px;
   font-weight: 500;
   font-size: 32px;
   line-height: 1.17;
+  text-transform: uppercase;
   font-family: ${props => props.theme.fonts.first};
   color: ${props => props.theme.main.textColor};
 `;
