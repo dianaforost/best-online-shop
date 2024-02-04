172687250f1914bb98f11a3d11f2bb255cd71ee2
barkode <konstantinks@gmail.com>
2024-02-04 16:30:32 +0000

    Add re-export images from index.js

---
+++ src/images/index.js
@@ -0,0 +1,61 @@
+//Main
+
+import heroBgFirst from './Hero/hero-bg-first.jpg';
+import heroBgFirst2x from './Hero/hero-bg-first@2x.jpg';
+import heroBgSec from './Hero/hero-bg-sec.jpg';
+import heroBgSec2x from './Hero/hero-bg-sec@2x.jpg';
+import heroBgThird from './Hero/hero-bg-third.jpg';
+import heroBgThird2x from './Hero/hero-bg-third@2x.jpg';
+
+// Top banner
+import banner1 from './banner/banner1.jpg';
+import banner1x2 from './banner/banner1@2x.jpg';
+
+// Bottom banner
+import banner2 from './banner/banner2.jpg';
+import banner2x2 from './banner/banner2@2x.jpg';
+
+//Catalog page
+
+import forHer from './for-her.png';
+import forHim from './for-him.png';
+import novelties from './novelties.png';
+import all from './all.png';
+
+// Gender section
+
+import forHerFirst from './for-her-first.png';
+import forHerSec from './for-her-second.png';
+import forHimFirst from './for-him-first.png';
+import forHimSec from './for-him-second.png';
+import winterForHer from './winter-for-her.png';
+import winterForHim from './winter-for-him.png';
+
+// Product component
+
+import image from './Rectangle.jpg';
+
+// Export all images
+export {
+  image,
+  heroBgFirst,
+  heroBgFirst2x,
+  heroBgSec,
+  heroBgSec2x,
+  heroBgThird,
+  heroBgThird2x,
+  banner1,
+  banner1x2,
+  banner2,
+  banner2x2,
+  all,
+  forHer,
+  forHim,
+  novelties,
+  forHerFirst,
+  forHerSec,
+  forHimFirst,
+  forHimSec,
+  winterForHer,
+  winterForHim,
+};
