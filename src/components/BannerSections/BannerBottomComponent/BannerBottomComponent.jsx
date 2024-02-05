792e28b53c817b43d31cc33e9b54ad7ad544f3b3
barkode <konstantinks@gmail.com>
2024-02-05 23:47:57 +0000

    Update BannerBottomComponent.jsx

--- src/components/BannerSections/BannerBottomComponent/BannerBottomComponent.jsx
+++ src/components/BannerSections/BannerBottomComponent/BannerBottomComponent.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { BannerWrapper } from '../BannerWrapper/BannerWrapper';
 
-export const BannerBottomComponent = () => {
-  return <div>BannerBottomComponent</div>;
+export const BannerBottomComponent = props => {
+  return (
+    <BannerWrapper
+      {...props}
+      $style={{
+        paddingTop: '40px',
+        paddingRight: '16px',
+        paddingBottom: '120px',
+        paddingLeft: '96px',
+      }}
+    >
+      <div>
+        <p> -20% </p>
+        <h2> Знижки </h2>
+        <p> -50% </p>
+        <p> -30% </p>
+        <h3>Купуй та вигравай приємні подарунки</h3>
+      </div>
+    </BannerWrapper>
+  );
 };
