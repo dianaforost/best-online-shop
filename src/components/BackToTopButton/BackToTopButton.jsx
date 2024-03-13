5a6cc9d6bb904f73366810afff15a0e3ca766684
barkode <konstantinks@gmail.com>
2024-03-13 00:01:22 +0000

    create component-button back to top

---
+++ src/components/BackToTopButton/BackToTopButton.jsx
@@ -0,0 +1,3 @@
+export const BackToTopButton = () => {
+  return <div></div>;
+};
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-
+import { BackToTopButton } from 'components/BackToTopButton/BackToTopButton';
+import { Footer } from 'components/Footer/Footer';
 import { Header } from 'components/Header/Header';
 import { Outlet } from 'react-router-dom';
-import { Footer } from 'components/Footer/Footer';
 
 export const SharedLayout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <BackToTopButton />
       <Footer />
     </>
   );
