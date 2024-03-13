ab1a86f11427c2d5233711c13b0b31490f3b7164
barkode <konstantinks@gmail.com>
2024-03-13 14:34:29 +0000

    Update BackToTopButton.styled.jsx

--- src/components/BackToTopButton/BackToTopButton.styled.jsx
+++ src/components/BackToTopButton/BackToTopButton.styled.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: fixed;
+  z-index: 10000;
   bottom: 50px;
   right: 50px;
   background-color: green;
@@ -11,8 +12,11 @@ export const Container = styled.div`
 `;
 
 export const Button = styled.button`
-  height: 100%;
-  width: 100%;
   font-size: 24px;
+  width: 100%;
+  height: 100%;
   color: red;
+  background-color: inherit;
+  border: none;
+  line-height: 0;
 `;
